8ca928603b2f64f3809e9f2640cb730e9f43e3a0
Zachary Ang Zi Yang <86767674+ZacharyAng0309@users.noreply.github.com>
2024-07-09 11:41:53 +0800

    Backend DAH COMPLETE

---
+++ DiningHub/Controllers/CategoriesController.cs
@@ -0,0 +1,145 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DiningHub.Models;
+using Microsoft.AspNetCore.Authorization;
+using DiningHub.Areas.Identity.Data;
+
+namespace DiningHub.Controllers
+{
+    [Authorize(Policy = "RequireInternalRole")]
+    [Route("manage/categories")]
+    public class CategoriesController : Controller
+    {
+        private readonly DiningHubContext _context;
+
+        public CategoriesController(DiningHubContext context)
+        {
+            _context = context;
+        }
+
+        // GET: manage/categories
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Categories.ToListAsync());
+        }
+
+        // GET: manage/categories/create
+        [HttpGet("create")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: manage/categories/create
+        [HttpPost("create")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("CategoryId,Name")] Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(category);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+
+        // GET: manage/categories/edit/5
+        [HttpGet("edit/{id}")]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _context.Categories.FindAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        // POST: manage/categories/edit/5
+        [HttpPost("edit/{id}")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("CategoryId,Name")] Category category)
+        {
+            if (id != category.CategoryId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(category);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CategoryExists(category.CategoryId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+
+        // GET: manage/categories/delete/5
+        [HttpGet("delete/{id}")]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _context.Categories
+                .FirstOrDefaultAsync(m => m.CategoryId == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        // POST: manage/categories/delete/5
+        [HttpPost("delete/{id}"), ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var category = await _context.Categories
+                .Include(c => c.MenuItems)
+                .Include(c => c.InventoryItems)
+                .FirstOrDefaultAsync(c => c.CategoryId == id);
+
+            if (category.MenuItems.Any() || category.InventoryItems.Any())
+            {
+                TempData["AlertMessage"] = "Cannot delete category as it has associated menu items or inventory items.";
+                return RedirectToAction(nameof(Index));
+            }
+
+            _context.Categories.Remove(category);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CategoryExists(int id)
+        {
+            return _context.Categories.Any(e => e.CategoryId == id);
+        }
+    }
+}
